ac4656e3cb694b52789ebb8510e626f9959e850e
BasedBerry <beaglechessdebate2@gmail.com>
2023-09-18 18:40:16 -0400

    jajajaajaj

---
+++ src/MultiSet.java
@@ -0,0 +1,16 @@
+public interface MultiSet<T>{
+
+     boolean add(T item);
+
+
+     void remove (T item);
+
+     boolean contains (T item);
+
+     boolean is_empty (T item);
+
+     int count (T item);
+
+     int size (T item);
+
+}
